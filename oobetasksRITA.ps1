4ab6b82731701718de2fb398e02e7767dd8c6a53
HenrikPost <henpo@itm8.com>
2026-02-25 12:37:47 +0100

    Create oobetasksRITA.ps1

---
+++ oobetasksRITA.ps1
@@ -0,0 +1,121 @@
+# oobetasks.osdcloud.ch
+
+$scriptFolderPath = "$env:SystemDrive\OSDCloud\Scripts"
+$ScriptPathOOBE = $(Join-Path -Path $scriptFolderPath -ChildPath "OOBE.ps1")
+$ScriptPathSendKeys = $(Join-Path -Path $scriptFolderPath -ChildPath "SendKeys.ps1")
+
+If(!(Test-Path -Path $scriptFolderPath)) {
+    New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
+}
+
+$OOBEScript =@"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
+
+Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
+
+Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Product Key Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://gist.githubusercontent.com/DelPost/e3542596fbdffdb872d68074e31b9b6f/raw/358709149f6c4b32d225487525c4925a6f9b5a10/Install-EmbeddedProductKey.ps1" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Autopilot Check Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://gist.githubusercontent.com/DelPost/b76fffec50cbe69a693a91eee1c1d2b1/raw/dfef85435e211a1c46ac07afabb35c54342e602b/AP-Prereq.ps1" -Wait
+
+Write-Host -ForegroundColor DarkGray "Installing Get-WindowsAutopilotInfo module"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://gist.githubusercontent.com/DelPost/d0318b5894a3ec7d262c57f89aa05035/raw/78cbaaabac5cc44f9db49d3296ff4139f51f0440/AddAutopilotDeviceRITA.ps1" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing OOBEDeploy Script from OSDCloud Module"
+Start-Process PowerShell -ArgumentList "-NoL -C Start-OOBEDeploy" -Wait
+
+Write-Host -ForegroundColor DarkGray "Executing Cleanup Script"
+Start-Process PowerShell -ArgumentList "-NoL -C Invoke-WebPSScript https://gist.githubusercontent.com/DelPost/3c208aceb40b8083f0bc043c0097bf0c/raw/463a7ef49d23fd0ee1f47d5b728b0aec53f2cc62/CleanUp.ps1" -Wait
+
+# Cleanup scheduled Tasks
+Write-Host -ForegroundColor DarkGray "Unregistering Scheduled Tasks"
+Unregister-ScheduledTask -TaskName "Scheduled Task for SendKeys" -Confirm:`$false
+Unregister-ScheduledTask -TaskName "Scheduled Task for OSDCloud post installation" -Confirm:`$false
+
+Write-Host -ForegroundColor DarkGray "Restarting Computer"
+Start-Process PowerShell -ArgumentList "-NoL -C Restart-Computer -Force" -Wait
+
+Stop-Transcript -Verbose | Out-File
+"@
+
+Out-File -FilePath $ScriptPathOOBE -InputObject $OOBEScript -Encoding ascii
+
+$SendKeysScript = @"
+`$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-SendKeys.log"
+Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
+
+Write-Host -ForegroundColor DarkGray "Stop Debug-Mode (SHIFT + F10) with WscriptShell.SendKeys"
+`$WscriptShell = New-Object -com Wscript.Shell
+
+# ALT + TAB
+Write-Host -ForegroundColor DarkGray "SendKeys: ALT + TAB"
+`$WscriptShell.SendKeys("%({TAB})")
+
+Start-Sleep -Seconds 1
+
+# Shift + F10
+Write-Host -ForegroundColor DarkGray "SendKeys: SHIFT + F10"
+`$WscriptShell.SendKeys("+({F10})")
+
+Stop-Transcript -Verbose | Out-File
+"@
+
+Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding ascii
+
+# Download ServiceUI.exe
+Write-Host -ForegroundColor Gray "Download ServiceUI.exe from GitHub Repo"
+Invoke-WebRequest https://github.com/DelPost/oobetasks/raw/refs/heads/main/ServiceUI64.exe -OutFile "C:\OSDCloud\ServiceUI.exe"
+
+#Create Scheduled Task for SendKeys with 15 seconds delay
+$TaskName = "Scheduled Task for SendKeys"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT15S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathSendKeys + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
+
+# Create Scheduled Task for OSDCloud post installation with 20 seconds delay
+$TaskName = "Scheduled Task for OSDCloud post installation"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT20S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathOOBE + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
